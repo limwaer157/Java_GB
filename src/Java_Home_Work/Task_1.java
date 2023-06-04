da044bbb5febb3a77a376eccb6358cab448c30a9
Denis <Limwaer157@gmail.com>
2023-06-04 16:45:11 -0600

    first Task

--- src/Java_Home_Work/Task_1.java
+++ src/Java_Home_Work/Task_1.java
@@ -1,4 +1,21 @@
 package Java_Home_Work;
 
+//Дана последовательность целых чисел, оканчивающаяся нулем. Найти сумму положительных чисел, после которых следует отрицательное число.
+//        Пример ввода:
+//        1 2 1 2 -1 1 3 1 3 -1 0
 public class Task_1 {
+    public static void main(String[] args) {
+    int []array = {1,2,1,2,-1,1,3,1,3,-1,0};
+        System.out.print(summ(array));
+    }
+    static int summ(int []nums)
+    {
+        int result = 0;
+        for (int i = 0; i < nums.length; i++)
+        {
+            if(nums[i] < 0)
+                result = result + nums[i-1];
+        }
+        return result;
+    }
 }
